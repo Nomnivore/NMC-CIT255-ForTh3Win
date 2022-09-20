134c0d317c086ebfe8c70d3939aca7abe0bef00c
RiggityRussell <russellgtarlt@gmail.com>
2022-09-20 17:24:16 -0400

    Scaffolding Russell

---
+++ Pages/Russell/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ForTh3Win.Data;
+using ForTh3Win.Models;
+
+namespace ForTh3Win.Pages.Russell
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ForTh3Win.Data.ForTh3WinContext _context;
+
+        public CreateModel(ForTh3Win.Data.ForTh3WinContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Review Review { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Review.Add(Review);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
