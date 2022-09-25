db66b0f6daed0dbf69de4a26081f40b5fec605e1
RiggityRussell <russellgtarlt@gmail.com>
2022-09-25 19:55:40 -0400

    Russell Pages Created

--- Pages/Russell/Index.cshtml.cs
+++ Pages/Russell/Index.cshtml.cs
@@ -24,11 +24,12 @@ namespace ForTh3Win.Pages.Russell
         public IList<Review> Review { get; set; } = default!;
         [BindProperty(SupportsGet = true)]
         public string? SearchString { get; set; }
-        public SelectList? Genre { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? Genre { get; set; }
         [BindProperty(SupportsGet = true)]
         public string? Titles { get; set; }
 
-       // public IList<Review> Review { get;set; } = default!;
+        // public IList<Review> Review { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
@@ -39,12 +40,28 @@ namespace ForTh3Win.Pages.Russell
 
             var title = from m in _context.Review
                          select m;
+
+           
             if (!string.IsNullOrEmpty(SearchString))
             {
                 title = title.Where(s => s.GameName.Contains(SearchString));
             }
+            
+            if (Genre.HasValue && Enum.IsDefined(typeof(GenreEnum), Genre))
+            {
+                var genre = (GenreEnum)Genre;
 
+                title = title.Where(x => x.Genre == genre);
+            }
+
+
+           /* Genre = new SelectList(await title.Distinct().ToListAsync());*/
             Review = await title.ToListAsync();
+
+          
+
         }
+
+       
     }
 }
