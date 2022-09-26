12ea8ff309a408118d70a55730034f4c95ae8763
trukhaibit <lil.oski.one@gmail.com>
2022-09-26 19:36:21 -0400

    Completed Search Tool

--- Pages/James/Index.cshtml.cs
+++ Pages/James/Index.cshtml.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ForTh3Win.Data;
 using ForTh3Win.Models;
+using System.Data;
+using Microsoft.Data.SqlClient.DataClassification;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ForTh3Win.Pages.James
 {
@@ -19,14 +22,37 @@ namespace ForTh3Win.Pages.James
             _context = context;
         }
 
+        public string SearchSort { get; set; }
+        public ESRBEnum ESRBSort { get; set; }
+        public GenreEnum GenreSort { get; set; }
+
         public IList<Review> Review { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString, string esrbSearch, string genreSearch)
         {
+            
+            IQueryable<Review> reviewList = from r in _context.Review select r;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                SearchSort = searchString;
+                reviewList = reviewList.Where(r => r.GameName.Contains(SearchSort));
+            }
+            if (!String.IsNullOrEmpty(esrbSearch))
+            {
+                ESRBSort = (ESRBEnum)Enum.Parse(typeof(ESRBEnum), esrbSearch);
+                reviewList = reviewList.Where(r => r.ESRBRating == ESRBSort);
+            }
+            if (!String.IsNullOrEmpty(genreSearch))
+            {
+                GenreSort = (GenreEnum)Enum.Parse(typeof(GenreEnum), genreSearch);
+                reviewList = reviewList.Where(r => r.Genre == GenreSort);
+            }
             if (_context.Review != null)
             {
-                Review = await _context.Review.ToListAsync();
+                Review = await reviewList.AsNoTracking().ToListAsync();
             }
+            return filtered;
+            return filtered;
         }
-    }
+    } 
 }
